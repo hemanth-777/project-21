febb23870ecaae32d1899fc1c8722e22b8aad2e8
hemanth-777 <74964414+hemanth-777@users.noreply.github.com>
2021-01-21 13:30:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+var wall,thickness;
+var bullet,speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,83)
+  speed=random(223,321);
+  weight=random(30,52);
+  
+ 
+ wall = createSprite(1200,200,thickness,height/2);
+ 
+ 
+}
+
+function draw() {
+  background("blue");  
+
+  
+
+  
+if(hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed * speed/(thickness *thickness *thickness);
+
+  if(damage>10)
+  {
+    wall.shapecolour=color(255,0,0);
+  }
+
+
+  if(damage<10)
+  {
+    wall.shapeColor=color(255,0,0);
+  }
+}
+drawSprites();
+}
+
+function hasCollided(Lbullet,Lwall)
+{
+   bulletRightEdge=lbullet.x +lbullet.width;
+   wallLeftEdge=lwall.x;
+   if (bulletRightEdge>=wallLeftEdge)
+   {
+     return true
+
+   }
+   return false;
+
+
+}
